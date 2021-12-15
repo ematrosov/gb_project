c4b0352010163c24fbc231e1837571c77fe1a75b
Marina <churmika@gmail.com>
2021-12-15 14:29:24 +0300

    add js

---
+++ src/vuex/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store({
+    state: {},
+    mutations: {},
+    getters: {},
+    actions: {}    
+});
+
+export default store;
