7d65b752fad6e9e31d33ac3424cb16a18c85a32e
ArturPravdivtsev <artiking@mail.ru>
2021-12-16 13:27:37 +0300

    Добавил страницу в роутер

--- src/router/router.js
+++ src/router/router.js
@@ -3,18 +3,23 @@ import VueRouter from 'vue-router'
 
 import HelloWorld from '../pages/HelloWorld'
 import Find from '../pages/Find'
+import Reviews from '../pages/Reviews'
 
 Vue.use(VueRouter);
 
 let router = new VueRouter({
     routes: [
-        { 
+        {
             path: '',
             component: HelloWorld
         },
-        { 
+        {
             path: '/find-sitter',
             component: Find
+        },
+        {
+            path: '/reviews',
+            component: Reviews
         }
     ],
     mode: 'history'
