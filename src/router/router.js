51e5b101e7ca8da42f959f16cb158fb9c1bdfba7
matrosov <egor.matrosov@gmail.com>
2021-12-21 11:46:36 +0700

    Добавил страницу О нас

---
+++ src/router/router.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Main from '../components/Main'
+//import HelloWorld from '../pages/HelloWorld'
+import Find from '../pages/Find'
+import Reviews from '../pages/Reviews'
+import About from '../pages/About'
+import Become from '../pages/Become'
+
+Vue.use(VueRouter);
+
+console.log('вошла в роутер')
+
+let router = new VueRouter({
+    routes: [
+        {
+            path: '',
+            component: Main
+        },
+        //{
+        //path: '',
+        //component: HelloWorld
+        //},
+        {
+            path: '/find-sitter',
+            component: Find
+        },
+        {
+            path: '/reviews',
+            component: Reviews
+        },
+        {
+            path: '/become-sitter',
+            component: Become
+        },
+        {
+            path: '/about',
+            component: About
+        }
+    ],
+    mode: 'history'
+})
+
+export default router;
