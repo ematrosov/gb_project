6edbf17b7d5ed0add7bb993a9f1e8d8ca997c7bc
Marina <churmika@gmail.com>
2021-12-27 12:58:43 +0300

    profile

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,24 +1,56 @@
 <template>
-  <div>
-    <div>
-      <div>
-        <img src="" alt="Фото профиля" />
-      </div>
-      <div>
-        <p>Имя</p>
-        <p>Фамилия</p>
-        <p>Дата рождения</p>
-        <p>Город</p>
-      </div>
+  <div class="profile">
+    <div class="profile_img">
+      <img class="profile_img__image" src="#" alt="Фото профиля" />
     </div>
-    <div>
-      <p>Расскажите о себе</p>
+    <div class="profile_info">
+      <p>{{ this.name }}</p>
+      <p>{{ this.surname }}</p>
+      <p>{{ this.dateBirthday }}</p>
+      <p>{{ this.city }}</p>
     </div>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      name: "Marina",
+      surname: "Churmeeva",
+      dateBirthday: "21.09.1993",
+      city: "Nizny Novgorod",
+    };
+  },
+};
 </script>
 
 <style>
+.profile {
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 125px;
+}
+
+.profile_img {
+  width: 200px;
+  height: 200px;
+  padding-top: 80px;
+  border: 1px solid black;
+  box-sizing: border-box;
+  border-radius: 50%;
+  text-align: center;
+}
+
+.profile_img__image {
+  height: 100%;
+  width: 100%;
+}
+
+.profile_info {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
 </style>
